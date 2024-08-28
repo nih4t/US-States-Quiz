31bfc3ab75efd137c18c1ea534357af8856aae03
nih4t <nihatkky@gmail.com>
2024-08-29 00:09:23 +0200

    improvements

--- main.py
+++ main.py
@@ -17,10 +17,7 @@ while len(correct_answers) < 50:
     answer_state = screen.textinput(title=f"{len(correct_answers)}/50 Guess the State",
                                     prompt="What's another state's name?").title()
     if answer_state == "Exit":
-        missing_states = []
-        for state in states:
-            if state not in correct_answers:
-                missing_states.append(state)
+        missing_states = [state for state in states if state not in correct_answers]
         pd.DataFrame(missing_states).to_csv("states_to_learn.csv")
         break
     if answer_state in states and answer_state not in correct_answers:
@@ -28,6 +25,5 @@ while len(correct_answers) < 50:
         state_data = data[data.state == answer_state]
         turtle.goto(state_data.x.item(), state_data.y.item())
         turtle.write(answer_state)
-
 
 
